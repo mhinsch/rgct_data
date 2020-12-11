3371033f87492330be28aef0e49c194013b4190d
Martin Hinsch <hinsch.martin@gmail.com>
2020-12-11 11:37:54 +0000

    upstream changes in SimpleAgentEvents

--- SimpleAgentEvents/src/SimpleAgentEvents.jl
+++ SimpleAgentEvents/src/SimpleAgentEvents.jl
@@ -1,11 +1,13 @@
 module SimpleAgentEvents
 
-export @processes, Scheduler
+export @processes, @add_processes, Scheduler
 
 
 using MacroTools
 using Distributions
 using StaticArrays
+
+include("Scheduler.jl")
 
 
 # TODO
@@ -15,24 +17,9 @@ using StaticArrays
 # in which module should rand. println, etc. be executed?
 
 
-macro processes(model_name, sim, agent_decl, decl)
-	# some superficial sanity checks
-	if ! isexpr(agent_decl, Symbol("::"))
-		error("@processes expects an agent declaration as 3rd argument")
-	end
-
-	if typeof(decl) != Expr || decl.head != :block
-		error("@processes expects a declaration block as 4th argument")
-	end
-
-	agent_name = agent_decl.args[1]
-	agent_type = agent_decl.args[2]
-
+function parse_declarations(lines)
 	pois = []
-	dir = []
 
-	# sort by distributions
-	lines = decl.args
 	for line in lines
 		# filter out line numbers
 		if typeof(line) == LineNumberNode
@@ -59,51 +46,53 @@ macro processes(model_name, sim, agent_decl, decl)
 		end
 	end
 
+	pois
+end
 
-	# name functions by model so that different models on the same type
-	# can be used in parallel
-	pois_func_name = Symbol("process_poisson_" * String(model_name))
+
+# add all poisson events
+function build_poisson_function(poisson_actions, func_name, model_name, agent_name, agent_type, sim)
 
 	# general bits of the function body
-	pois_func = :(function $(esc(pois_func_name))($(esc(agent_decl)), $(esc(:sim)))
-			rates = zeros(MVector{$(length(pois))})
+	func = :(function $(esc(model_name)).$func_name($(esc(agent_name)) :: $(esc(agent_type)), $(esc(sim)))
+			rates = zeros(MVector{$(length(poisson_actions))})
 		end)
-	
-	pois_func_body = pois_func.args[2].args
+
+	func_body = func.args[2].args
+
 	action_ifs = []
 
-	# add all poisson events
 	i = 1
-	for (d, a) in pois
-		rate = d.args[3]
-
+	for (d, a) in poisson_actions
 		if !iscall(a, :(=>))
 			error("event declaration expected: @<DISTR>(<RATE>) ~ COND => ACTION")
 		end
 
+		rate = d.args[3]
+
 		cond_act = rmlines(a.args)
-		cond = cond_act[2]
+		condition = cond_act[2]
 		action = cond_act[3]
 
 		# condition check
-		check = :(if $(esc(cond))
+		check = :(if $(esc(condition))
 				rates[$i] = $(esc(rate))
 			end)
-		push!(pois_func_body, check)
+		push!(func_body, check)
 
 		# check if selected, execute
 		ai = :(
 			if rnd < rates[$i]
 #				println("@ ", w_time, " -> ", $(string(action)))
 
-				$(esc(:schedule_in!))($(esc(agent_name)), w_time, $(esc(:scheduler))($(esc(sim)))) do $(esc(agent_name))
+				$(esc(model_name)).schedule_in!($(esc(agent_name)), w_time) do $(esc(agent_name))::$(esc(agent_type))
 					active = $(esc(action))
 
 					for obj in active 
 						# should not be needed as queue as well as actions are unique in 
 						# agents
 						# $(esc(:unschedule!))($(esc(:scheduler))($(esc(sim))), obj)
-						$(esc(pois_func_name))(obj, $sim)
+						$(esc(model_name)).$func_name(obj, $sim)
 					end
 				end
 
@@ -119,7 +108,7 @@ macro processes(model_name, sim, agent_decl, decl)
 	end
 
 	# bits between conditions and selection
-	push!(pois_func_body, :(rate = $(esc(:sum))(rates);
+	push!(func_body, :(rate = $(esc(:sum))(rates);
 		if rate == 0.0
 			return
 		end;
@@ -127,30 +116,88 @@ macro processes(model_name, sim, agent_decl, decl)
 		rnd = rand() * rate
 		))
 
-	append!(pois_func_body, action_ifs)
+	append!(func_body, action_ifs)
 
 	# if we didn't select *any* action something went wrong
-	push!(pois_func_body, :(println("No action selected! ", rnd, " ", rate)))
+	push!(func_body, :(println("No action selected! ", rnd, " ", rate);return))
 
+	func
+end
 
-	# the entire bunch of code
-	ret = Expr(:block)
+function build_spawn_func(func_name, model_name, pois_func_name, agent_type)
+	:(
+	function $(esc(model_name)).$func_name(agent::$(esc(agent_type)), sim)
+		$(esc(model_name)).$pois_func_name(agent, sim)
+	end
+	)
+end
 
-	push!(ret.args, pois_func)
+pois_func_name() = :process_poisson
+spawn_func_name() = :spawn
 
-	# push spawn second, nice for interactive use as it shows the function
-	# name as output from the macro call
-	spawn_func_name = Symbol("spawn_" * String(model_name))
-	# and we also need a function to get an agent started
-	push!(ret.args, :(function $(esc(spawn_func_name))(agent::$(esc(agent_type)), sim)
-			$(esc(pois_func_name))(agent, sim)
-		end
-		))
+function gen_functions(model_name, sim, agent_decl, decl)
+	# some superficial sanity checks
+	@capture(agent_decl, agent_name_ :: agent_type_) ||
+		error("@processes expects an agent declaration as 3rd argument")
 
-	ret
+	if typeof(decl) != Expr || decl.head != :block
+		error("@processes expects a declaration block as 4th argument")
+	end
+
+	# sort by distributions
+	pois = parse_declarations(decl.args)
+
+	# *** scheduling function
+	pois_func = build_poisson_function(pois, pois_func_name(), model_name, agent_name, agent_type, sim)
+
+	# *** and we also need a function to get an agent started
+	spawn_func = build_spawn_func(spawn_func_name(), model_name, pois_func_name(), agent_type)
+
+	pois_func, spawn_func
 end
 
 
-include("Scheduler.jl")
+macro processes(model_name, sim, agent_decl, decl)
+	pois_func, spawn_func = gen_functions(model_name, sim, agent_decl, decl)
+
+	pfn = pois_func_name()
+	sfn = spawn_func_name()
+
+	# the entire bunch of code
+	mod = :(module $(esc(model_name)) 
+			using SimpleAgentEvents
+			import SimpleAgentEvents.Scheduler
+			const SC = SimpleAgentEvents.Scheduler
+
+			export $(esc(pfn)), $(esc(sfn))
+
+			const scheduler = SC.PQScheduler{Float64}()
+			$(esc(:isempty))() = SC.isempty(scheduler)
+			$(esc(:schedule!))(fun, obj, at) = SC.schedule!(fun, obj, at, scheduler)
+			$(esc(:time_now))() = SC.time_now(scheduler)
+			$(esc(:time_next))() = SC.time_next(scheduler)
+			$(esc(:schedule_in!))(fun, obj, t) = SC.schedule_in!(fun, obj, t, scheduler)
+			$(esc(:next!))() = SC.next!(scheduler)
+			$(esc(:upto!))(atime) = SC.upto!(scheduler, atime)
+			$(esc(:unschedule!))(obj) = SC.unschedule!(obj, scheduler) 
+			$(esc(:reset!))() = SC.reset!(scheduler)
+			$(esc(:scheduler))() = scheduler
+		end)
+
+	mod_body = mod.args[3].args
+	push!(mod_body, esc(Expr(:function, pfn)))
+	push!(mod_body, esc(Expr(:function, sfn)))
+
+	Expr(:toplevel, mod, pois_func, spawn_func)
+end
+
+
+macro add_processes(model_name, sim, agent_decl, decl)
+	pois_func, spawn_func = gen_functions(model_name, sim, agent_decl, decl)
+
+	Expr(:toplevel, pois_func, spawn_func)
+end
+
+
 
 end
--- SimpleAgentEvents/test/test_sched.jl
+++ SimpleAgentEvents/test/test_sched.jl
@@ -1,24 +1,22 @@
 push!(LOAD_PATH, "./src")
 
 using SimpleAgentEvents
-using SimpleAgentEvents.Scheduler
-
-
-mutable struct Model
-end
-
-struct Simulation
-	scheduler :: PQScheduler{Float64}
-	model :: Model
-end
-
-scheduler(sim) = sim.scheduler
 
 
 mutable struct A1
 	id :: Int
 	state :: Int
 	food :: Int
+end
+
+mutable struct Model
+	pop :: Vector{A1}
+end
+
+Model() = Model([])
+
+struct Simulation
+	model :: Model
 end
 
 
@@ -63,29 +61,38 @@ function fallasleep(a::A1, model)
 end
 
 
-const sim = Simulation(PQScheduler{Float64}(), Model())
+const simulation = Simulation(Model())
 
 
-@processes SimpleTest sim self::A1 begin
+@processes SimpleTest simulation self::A1 begin
 	# wake up
-	@poisson(2.0)	~ self.state == 0					=> wakeup(self, sim.model)
-	@poisson(1.0)	~ self.state == 0					=> sleep(self, sim.model)
-	@poisson(0.5)	~ self.state == 1 && self.food > 3	=> walk(self, sim.model)
-	@poisson(1.0) 	~ self.state == 1 && self.food <= 3	=> forage(self, sim.model)
-	@poisson(3.0)	~ self.state == 1 && self.food > 1	=> fallasleep(self, sim.model)
+	@poisson(2.0)	~ self.state == 0					=> wakeup(self, simulation.model)
+	@poisson(1.0)	~ self.state == 0					=> sleep(self, simulation.model)
+	@poisson(0.5)	~ self.state == 1 && self.food > 3	=> walk(self, simulation.model)
+	@poisson(1.0) 	~ self.state == 1 && self.food <= 3	=> forage(self, simulation.model)
+	@poisson(3.0)	~ self.state == 1 && self.food > 1	=> fallasleep(self, simulation.model)
 end
 
-function setup(n)
-	for i in 1:n
-		spawn_SimpleTest(A1(i), sim)
+@add_processes SimpleTest simulation m::Model begin
+	@poisson(10.0) ~ length(m.pop) < 10 => begin
+		push!(m.pop, A1(1))
+		SimpleTest.spawn(m.pop[end], simulation)
+		println("added agent")
+		[m]
 	end
+end
+
+function setup()
+	SimpleTest.spawn(simulation.model, simulation)
 end
 
 function run(n)
 	for i in 1:n
-		next!(sim.scheduler)
+		SimpleTest.next!()
 	end
-	println(time_next(sim.scheduler))
+	println(SimpleTest.time_next())
 end
 
+setup()
 
+run(10)
