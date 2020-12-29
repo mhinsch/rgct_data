3ffa0c3e31a2409dd839ccb12dcf576e5a2cb010
Martin Hinsch <hinsch.martin@gmail.com>
2020-12-29 15:25:32 +0000

    move params <-> args code into module

--- base/args.jl
+++ base/args.jl
@@ -1,33 +1,3 @@
-using ArgParse
-using REPL
-
-"add all fields of a type as command line arguments"
-function fields_as_args!(arg_settings, t :: Type)
-	fields = fieldnames(t)
-	for f in fields
-		fdoc =  REPL.stripmd(REPL.fielddoc(t, f))
-		add_arg_table!(arg_settings, ["--" * String(f)], Dict(:help => fdoc))
-	end
-end
-
-"create object from command line arguments"
-function create_from_args(args, t :: Type)
-	par_expr = Expr(:call, t.name.name)
-
-	fields = fieldnames(t)
-
-	for key in eachindex(args)
-		if args[key] == nothing || !(key in fields)
-			continue
-		end
-		val = parse(fieldtype(t, key), args[key])
-		push!(par_expr.args, Expr(:kw, key, val))
-	end
-
-	eval(par_expr)
-end
-
-
 function get_parfile()
 	if length(ARGS) > 0 && ARGS[1][1] != '-'
 		parfile = ARGS[1]
@@ -37,18 +7,5 @@ function get_parfile()
 	end
 
 	parfile
-end
-
-
-function save_params(out_name, p)
-	open(out_name, "w") do out
-		println(out, "using Parameters")
-		println(out)
-		println(out, "@with_kw struct Params")
-		for f in fieldnames(typeof(p))
-			println(out, "\t", f, "\t::\t", fieldtype(typeof(p), f), "\t= ", getfield(p, f))
-		end
-		println(out, "end")
-	end
 end
 
