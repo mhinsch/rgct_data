dbf5987a0d1e44b2cba4511b645664d1239cafa1
Martin Hinsch <hinsch.martin@gmail.com>
2021-05-03 18:59:47 +0100

    more med stuff

--- scenarios/mediterranean.jl
+++ scenarios/mediterranean.jl
@@ -1,7 +1,16 @@
 mutable struct Scenario_med
 	"risks for sea crossings per year"
 	risks :: Vector{Float64}
+	"interception probability per year"
+	interc :: Vector{Float64}
+	"waiting time between crossing attempts"
+	wait :: Float64
+	"movement speed, copied from main params"
+	speed :: Float64
+	"list of all links that are sea crossings"
 	crossings :: Vector{Link}
+	"interception count per link"
+	interc_count :: Vector{Int}
 end
 
 Scenario_med() = Scenario_med([])
@@ -12,6 +21,8 @@ function setup_scenario_med(sim::Simulation, pars)
 		error("scenario data required")
 	end
 
+	scen.speed = pars.move_speed
+
 	# preliminary, needs changing later
 	scen.risks = parse.(Float64, pars)
 
@@ -20,18 +31,35 @@ function setup_scenario_med(sim::Simulation, pars)
 			link.l1.typ==EXIT || link.l2.typ==EXIT
 		end
 
+	scen.interc_count = zeros(Int, length(scen.crossings))
+
 	scen
 end
 
 function scenario_med!(scen, sim::Simulation, t)
-	year = floor(Int, t / 100) 
+	year = floor(Int, t / 100) + 1
 
 	@assert year <= length(scen.risks)
 
+	p_i = scen.interc[year]
+	# additional time for crossings
+	# being intercepted adds constant s to effective distance (waiting time), then
+	# the same process happens again
+	# for a given probability p_i to be intercepted the expected duration is therefore:
+	# E(d') = (1-p_i) d + p_i * (s + (1-p_i) d + p_i * (s + ...
+	# = d + p_i/(1-p_i) s
+	exp_time = p_i/(1.0-p_i) * scen.wait
+	n_i = 1/(1-p_i) - 1
+
 	risk = scen.risks[year]
 
-	for l in scen.crossings
+	for i in eachindex(scen.crossings)
+		l = scen.crossings[i]
+		# add distance so that it takes exp_time longer
+		l.friction = l.distance + exp_time * scen.speed
 		l.risk = risk
+		# calculate interceptions (cumulative)
+		scen.interc_count[i] = round(Int, n_i * l.count)
 	end
 end
 
