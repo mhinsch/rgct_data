9de00c5428ae400dc8ea3ed09a79d5f244db6d89
Martin Hinsch <hinsch.martin@gmail.com>
2021-05-06 09:28:52 +0100

    finish med scenario

--- scenarios/mediterranean.jl
+++ scenarios/mediterranean.jl
@@ -1,12 +1,14 @@
 mutable struct Scenario_med
-	"risks for sea crossings per year"
+	"risks for sea crossings per year (1st is warmup)"
 	risks :: Vector{Float64}
-	"interception probability per year"
+	"interception probability per year (1st is warmup)"
 	interc :: Vector{Float64}
 	"waiting time between crossing attempts"
 	wait :: Float64
 	"movement speed, copied from main params"
 	speed :: Float64
+	"how long to run before starting scenario"
+	warmup :: Float64
 	"list of all links that are sea crossings"
 	crossings :: Vector{Link}
 	"interception count per link"
@@ -14,22 +16,45 @@ mutable struct Scenario_med
 	outfile :: IO
 end
 
-Scenario_med() = Scenario_med([])
+Scenario_med() = Scenario_med([], [], 0, 0, 0, [], [], stdout)
 
 @observe log_medit scen begin
-	@show "intercept"	sum(scen.interc_count)
+	@show "n_interc"	sum(scen.interc_count)
 end
 
-function setup_scenario(::Type{Scenario_med}, sim::Simulation, pars)
+function setup_scenario(::Type{Scenario_med}, sim::Simulation, scen_args, pars)
 	scen = Scenario_med()
-	if length(pars) < 1
-		error("scenario data required")
-	end
+
+	as = ArgParseSettings("", autofix_names=true)
+
+	@add_arg_table! as begin
+		"--risks"
+			nargs = '+'
+			arg_type = Float64
+			required = true
+		"--int"
+			nargs = '+'
+			arg_type = Float64
+			required = true
+		"--wait"
+			arg_type = Float64
+			required = true
+		"--warmup"
+			arg_type = Float64
+			required = true
+		"--out"
+			default = "interceptions.txt"
+		end
+
+	args = parse_args(split(scen_args), as, as_symbols=true)
 
 	scen.speed = pars.move_speed
 
 	# preliminary, needs changing later
-	scen.risks = parse.(Float64, pars)
+	scen.risks = args[:risks]
+	scen.interc = args[:int]
+	scen.wait = args[:wait]
+	scen.warmup = args[:warmup]
 
 	# collect links to exits (== sea crossings)
 	scen.crossings = filter(sim.model.world.links) do link
@@ -38,14 +63,16 @@ function setup_scenario(::Type{Scenario_med}, sim::Simulation, pars)
 
 	scen.interc_count = zeros(Int, length(scen.crossings))
 
-	scen.outfile = open("med_output.txt")
+	scen.outfile = open(args[:out], "w")
 	print_header_log_medit(scen.outfile)
 
 	scen
 end
 
 function update_scenario!(scen::Scenario_med, sim::Simulation, t)
-	year = floor(Int, t / 100) + 1
+	year = t < scen.warmup ? 
+		1 :
+		floor(Int, (t-scen.warmup) / 100) + 2
 
 	@assert year <= length(scen.risks)
 
